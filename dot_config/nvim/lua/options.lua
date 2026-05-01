cf61f9a7d0dc41bef91d92b93397bdf834bb7032
AnTuA <108426137+xAnTuA@users.noreply.github.com>
2026-05-01 11:58:50 +0200

    feat(nvim): add NvChad config

---
+++ dot_config/nvim/lua/options.lua
@@ -0,0 +1,15 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+
+o.colorcolumn = "101"
+
+o.foldmethod = "indent"
+o.foldenable = true
+o.foldlevel = 99
+o.foldlevelstart = 99
+o.scrolloff = 10
+
+-- o.cursorlineopt ='both' -- to enable cursorline!
